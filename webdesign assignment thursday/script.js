ba0181f03807d06c7be5ffa9af5b6c2d61bc6ae0
IRAKOZE S Patrick <pvtrick>
2024-11-15 10:21:01 +0200

    Initial commit

---
+++ webdesign assignment thursday/script.js
@@ -0,0 +1,25 @@
+document.getElementById("home").addEventListener("click", function() {
+    document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("aboutUs").addEventListener("click", function() {
+    document.getElementById("section2").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("Skills").addEventListener("click", function() {
+    document.getElementById("section3").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("projects").addEventListener("click", function() {
+    document.getElementById("section4").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("contact").addEventListener("click", function() {
+    document.getElementById("section5").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("us").addEventListener("click", function() {
+    document.getElementById("section5").scrollIntoView({ behavior: "smooth" });
+});
+
+
